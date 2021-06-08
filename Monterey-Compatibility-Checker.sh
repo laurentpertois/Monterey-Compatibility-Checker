af2235d13d263eface35d1b045a76a721178c82f
Laurent Pertois <laurentpertois@users.noreply.github.com>
2021-06-08 02:45:21 +0200

    Initial commit
    Still in beta, waiting for more infos from Apple

---
+++ Monterey-Compatibility-Checker.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Copyright (c) 2021 Jamf.  All rights reserved.
+#
+#       Redistribution and use in source and binary forms, with or without
+#       modification, are permitted provided that the following conditions are met:
+#               * Redistributions of source code must retain the above copyright
+#                 notice, this list of conditions and the following disclaimer.
+#               * Redistributions in binary form must reproduce the above copyright
+#                 notice, this list of conditions and the following disclaimer in the
+#                 documentation and/or other materials provided with the distribution.
+#               * Neither the name of the Jamf nor the names of its contributors may be
+#                 used to endorse or promote products derived from this software without
+#                 specific prior written permission.
+#
+#       THIS SOFTWARE IS PROVIDED BY JAMF SOFTWARE, LLC "AS IS" AND ANY
+#       EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#       WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#       DISCLAIMED. IN NO EVENT SHALL JAMF SOFTWARE, LLC BE LIABLE FOR ANY
+#       DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#       (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#       LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#       ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#       (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#       SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# This script was designed to be used as an Extension Attribute to ensure specific
+# requirements have been met to deploy macOS Monterey.
+#
+# General Requirements:
+#		- OS X 10.9.0 or later (It seems, as of the day I write this Apple has not yet made recommendations, this page and the script will be adapted if necessary when the information will become public)
+#		- 4GB of memory (It seems, as of the day I write this Apple has not yet made recommendations, this page and the script will be adapted if necessary when the information will become public)
+#		- 20GB of available storage (It seems, as of the day I write this Apple has not yet made recommendations, this page and the script will be adapted if necessary when the information will become public)
+#
+#
+# These last 2 requirements can be modified in the first 2 variables (MINIMUMRAM
+# and MINIMUMSPACE).
+# 	- REQUIREDMINIMUMRAM: minimum RAM required, in GB
+# 	- REQUIREDMINIMUMSPACE: minimum disk space available, in GB. Monterey has different
+#							requirements depending on the OS from which you update
+#							Adjust to your needs, lines 79 (Catalina) or 82 (pre-Catalina) 
+#
+#
+# Mac Hardware Requirements and equivalent as minimum Model Identifier
+# 	- MacBook (Early 2015 or newer), ie MacBook8,1
+# 	- MacBook Pro (Early 2015 or newer), ie MacBookPro12,1
+# 	- MacBook Air (Early 2015 or newer), ie MacBookAir7,1
+# 	- Mac mini (Late 2014 or newer), ie Macmini7,1
+# 	- iMac (Late 2015 or newer), ie iMac16,1
+# 	- iMac Pro, ie iMacPro1,1
+# 	- Mac Pro (Late 2013 or newer), ie MacPro6,1
+#
+#
+# Default compatibility is set to False if no test pass (variable COMPATIBILITY)
+#
+# Written by: Laurent Pertois | Senior Professional Services Engineer | Jamf
+#
+# Created On: 2021-06-08
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Checks minimum version of the OS before upgrade (10.9.0)
+OSVERSIONMAJOR=$(sw_vers -buildVersion | cut -c 1-2)
+
+# Minimum RAM and Disk Space required (4GB and 45GB default. Note that REQUIREDMINIMUMSPACE must be set to an integer)
+# According to https://support.apple.com/en-us/HT211238 the minimum space requirement for Monterey is 35.5GB if you're coming from Sierra, it can go up to 44.5GB if coming from an older version
+# This value is acconting for the required space and the size of the installer (almost 13GB)
+REQUIREDMINIMUMRAM=4
+
+if [[ "$OSVERSIONMAJOR" -ge 16 ]]; then
+	# For Sierra and higher required space is 12.3GB for the installer and 35.5GB for required disk space for installation which equals to 47.8GB, 50GB is giving a bit of extra free space for safety
+	REQUIREDMINIMUMSPACE=50
+else
+	# For pre-Sierra required space is 12.3GB for the installer and 44.5GB for required disk space for installation which equals to 56.8GB, 60GB is giving a bit of extra free space for safety
+	REQUIREDMINIMUMSPACE=60
+fi
+
+#########################################################################################
+############### DO NOT CHANGE UNLESS NEEDED
+#########################################################################################
+
+# Default values for Compatibility is false
+COMPATIBILITY="False"
+
+#########################################################################################
+############### Let's go!
+#########################################################################################
+
+# Checks if computer meets pre-requisites for Monterey
+if [[ "$OSVERSIONMAJOR" -ge 13 && "$OSVERSIONMAJOR" -le 20 ]]; then
+
+	# Transform GB into Bytes
+	GIGABYTES=$((1024 * 1024 * 1024))
+	MINIMUMRAM=$(($REQUIREDMINIMUMRAM * $GIGABYTES))
+	MINIMUMSPACE=$(($REQUIREDMINIMUMSPACE * $GIGABYTES))
+
+	# Gets the Model Identifier, splits name and major version
+	MODELIDENTIFIER=$(/usr/sbin/sysctl -n hw.model)
+	MODELNAME=$(echo "$MODELIDENTIFIER" | sed 's/[^a-zA-Z]//g')
+	MODELVERSION=$(echo "$MODELIDENTIFIER" | sed -e 's/[^0-9,]//g' -e 's/,//')
+
+	# Gets amount of memory installed
+	MEMORYINSTALLED=$(/usr/sbin/sysctl -n hw.memsize)
+
+	# Gets free space on the boot drive
+	FREESPACE=$(diskutil info / | awk -F '[()]' '/Free Space|Available Space/ {print $2}' | sed -e 's/\ Bytes//')
+
+	# Checks if computer meets pre-requisites for Monterey
+	if [[ "$MODELNAME" == "iMac" && "$MODELVERSION" -ge 160 && "$MEMORYINSTALLED" -ge "$MINIMUMRAM" && "$FREESPACE" -ge "$MINIMUMSPACE" ]]; then
+		COMPATIBILITY="True"
+	elif [[ "$MODELNAME" == "iMacPro" && "$MODELVERSION" -ge 10 && "$MEMORYINSTALLED" -ge "$MINIMUMRAM" && "$FREESPACE" -ge "$MINIMUMSPACE" ]]; then
+		COMPATIBILITY="True"
+	elif [[ "$MODELNAME" == "Macmini" && "$MODELVERSION" -ge 70 && "$MEMORYINSTALLED" -ge "$MINIMUMRAM" && "$FREESPACE" -ge "$MINIMUMSPACE" ]]; then
+		COMPATIBILITY="True"
+	elif [[ "$MODELNAME" == "MacPro" && "$MODELVERSION" -ge 60 && "$MEMORYINSTALLED" -ge "$MINIMUMRAM" && "$FREESPACE" -ge "$MINIMUMSPACE" ]]; then
+	    COMPATIBILITY="True"
+	elif [[ "$MODELNAME" == "MacBook" && "$MODELVERSION" -ge 80 && "$MEMORYINSTALLED" -ge "$MINIMUMRAM" && "$FREESPACE" -ge "$MINIMUMSPACE" ]]; then
+	    COMPATIBILITY="True"
+	elif [[ "$MODELNAME" == "MacBookAir" && "$MODELVERSION" -ge 70 && "$MEMORYINSTALLED" -ge "$MINIMUMRAM" && "$FREESPACE" -ge "$MINIMUMSPACE" ]]; then
+	    COMPATIBILITY="True"
+	elif [[ "$MODELNAME" == "MacBookPro" && "$MODELVERSION" -ge 120 && "$MEMORYINSTALLED" -ge "$MINIMUMRAM" && "$FREESPACE" -ge "$MINIMUMSPACE" ]]; then
+	    COMPATIBILITY="True"
+	fi
+	# Outputs result
+	echo "<result>$COMPATIBILITY</result>"
+else
+	echo "<result>$COMPATIBILITY</result>"
+	exit $?
+fi
---
+++ README.md
@@ -0,0 +1,30 @@
+# Monterey-Compatibility-Checker
+
+**This is still a work in progress, I have not yet checked the prerequisites for disk space and minimum RAM so I kept the same as for macOS Big Sur. Once this information is made public, this will be adjusted.**
+
+This script was designed to be used as an Extension Attribute on Jamf Pro server to ensure specific requirements have been met to deploy macOS Monterey. With little modification it can probably be used on other systems (Jamf Pro server requires the output of an Extension Attribute to be `echo "<result>$FOO</result>`).
+
+## General Requirements:
+  - OS X 10.9.0 or later (It seems, as of the day I write this Apple has not yet made recommendations, this page and the script will be adapted if necessary when the information will become public)
+  - 4GB of memory (It seems, as of the day I write this Apple has not yet made recommendations, this page and the script will be adapted if necessary when the information will become public)
+  - 60GB of available storage, the required storage is different when upgrading from Catalina (33,5GB) or from older versions (44,5GB) and accounts for the size of the installer (almost 13GB)
+
+These last 2 requirements can be modified in the first 2 variables (`MINIMUMRAM` and `MINIMUMSPACE`).
+  - REQUIREDMINIMUMRAM: minimum RAM required, in GB
+  - REQUIREDMINIMUMSPACE: minimum disk space available, in GB
+ 
+
+## Mac Hardware Requirements and equivalent as minimum Model Identifier
+ 	- MacBook (Early 2015 or newer), ie MacBook8,1
+ 	- MacBook Pro (Early 2015 or newer), ie MacBookPro12,1
+ 	- MacBook Air (Early 2015 or newer), ie MacBookAir7,1
+ 	- Mac mini (Late 2014 or newer), ie Macmini7,1
+ 	- iMac (Late 2015 or newer), ie iMac16,1
+ 	- iMac Pro, ie iMacPro1,1
+ 	- Mac Pro (Late 2013 or newer), ie MacPro6,1
+
+Default compatibility is set to False if no test pass (variable `COMPATIBILITY`)
+
+## Installation
+
+Copy the content of the script (`.sh` file) to a new Computer Extension Attribute or just download the existing Extension Attribute (`.xml`) file and upload it to the Computer Extension Attributes of your Jamf Pro server.
